ad3743f92232321481d1c01813fa9622909708f0
nvhoang291299 <nvhoang020100@gmail.com>
2023-05-09 00:37:27 +0700

    nopbai

--- SS5_ORM/exam/src/main/java/com/example/exam/repository/impl/ProductRepository.java
+++ SS5_ORM/exam/src/main/java/com/example/exam/repository/impl/ProductRepository.java
@@ -75,22 +75,33 @@ public class ProductRepository implements IProductRepository {
 
     @Override
     public Product findById(int id) {
-
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        Product product = null;
+        try {
+            session = ConnectionUtil.sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            product = session.get(Product.class, id);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null){
+                session.close();
+            }
+        }
+        return product;
     }
 
     @Override
-    public void update(int product) {
+    public void update(Product product) {
         Session session = null;
         Transaction transaction = null;
 
         try {
-            session = ConnectionUtil.sessionFactory.getCurrentSession();
+            session = ConnectionUtil.sessionFactory.openSession();
             transaction = session.beginTransaction();
-            Product product = session.get(Product.class, idDel);
-            if (product != null){
-                session.delete(product);
-            }
+            session.saveOrUpdate(product);
             transaction.commit();
         } catch (Exception e){
             if (transaction != null){
@@ -105,7 +116,18 @@ public class ProductRepository implements IProductRepository {
 
     @Override
     public List<Product> findByName(String name) {
-        List<Product> products = new ArrayList<>();
-        return products;
+        Session session = null;
+        try {
+            session = ConnectionUtil.sessionFactory.getCurrentSession();
+            return session.createQuery("from Product where nameProduct=:name", Product.class)
+                    .setParameter("name", name).getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null){
+                session.close();
+            }
+        }
+        return null;
     }
 }
