e74ecae5125eaf02950d759f113d38e1f7d450bc
nvhoang291299 <nvhoang020100@gmail.com>
2023-05-14 21:06:58 +0700

    nopbai

--- SS8_validation/applicationBorrowBooks/src/main/java/com/example/applicationborrowbooks/exception/ExceptionBackBook.java
+++
@@ -1,4 +0,0 @@
-package com.example.applicationborrowbooks.exception;
-
-public class ExceptionBackBook extends Exception{
-}
--- SS8_validation/applicationBorrowBooks/src/main/java/com/example/applicationborrowbooks/repository/IOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.applicationborrowbooks.repository;
-
-import com.example.applicationborrowbooks.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IOrderRepository extends JpaRepository<Order, Long> {
-}
